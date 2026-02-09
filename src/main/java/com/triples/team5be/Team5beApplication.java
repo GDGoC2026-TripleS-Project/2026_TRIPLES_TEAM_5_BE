8432df4eed752c4fc0dacb31ce944c855e0aae0d
seung-yeonn <kimseungyeon0531@gmail.com>
2026-02-09 14:55:32 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/triples/team5be/Team5beApplication.java
@@ -0,0 +1,13 @@
+package com.triples.team5be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team5beApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team5beApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/triples/team5be/Team5beApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triples.team5be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team5beApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
